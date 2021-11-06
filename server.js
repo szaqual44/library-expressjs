25cec47f9ce6fe60c61c6299123a5a412d2f8ca6
szaqual44 <szaqual44@gmail.com>
2021-11-06 10:30:59 +0100

    new book works

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const expressLayouts = require('express-ejs-layouts')
 
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
+const bookRouter = require('./routes/books')
  
 
 app.set('view engine', 'ejs')
@@ -30,5 +31,6 @@ db.once('open', () => console.log('connected to Mongoose'))
 
 app.use('/', indexRouter)
 app.use('/authors', authorRouter)
+app.use('/books', bookRouter)
 
 app.listen(process.env.PORT || 3000)
